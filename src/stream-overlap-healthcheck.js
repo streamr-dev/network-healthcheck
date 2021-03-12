a8daa2e9d0977d258ec3be1fca6a2d4df54b3f15
juslesan <santeri.juslenius@gmail.com>
2021-03-12 13:04:58 +0200

    add error case if statement

--- src/stream-overlap-healthcheck.js
+++ src/stream-overlap-healthcheck.js
@@ -36,7 +36,7 @@ function parseResponseForFailures(res) {
             error: `Query to ${res.reason.config.url} failed, request timed out after ${queryTimeout}ms`,
             url: res.reason.config.url
         }
-    } else if (res.value.status !== 200) {
+    } else if (res.value.status !== 200 || res.value.data === {}) {
         return {
             error: `Query to ${res.value.config.url} failed with status ${res.value.status}`,
             url: res.value.config.url
